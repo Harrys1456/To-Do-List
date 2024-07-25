9db996fa298989fd5e9ccdd59ee8e9c3f4712b6f
unknown <mathon1456@gmail.com>
2024-07-25 19:46:40 -0400

    Added a task list with header and add task button

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MaterialsModule } from '../../materials.module';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [MaterialsModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
 
+    public task_list: string[] = [];
+
+    constructor(){}
+
+    public addTask(): void{
+      let test = "Test";
+      this.task_list.push(test);
+    }
 }
--- src/materials.module.ts
+++ src/materials.module.ts
@@ -15,6 +15,7 @@ import { MatDialogModule,
  import {MatMenuModule} from '@angular/material/menu';
  import {MatFormFieldModule} from '@angular/material/form-field';
  import {MatInputModule} from '@angular/material/input';
+ import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -36,7 +37,8 @@ import { MatDialogModule,
     MatListModule,
     MatMenuModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ],
   exports: [
     MatIconModule,
@@ -53,7 +55,8 @@ import { MatDialogModule,
     MatListModule,
     MatMenuModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ]
 })
 export class MaterialsModule { }
